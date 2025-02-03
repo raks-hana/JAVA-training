9cc9e5907d6aca7ec45714bdbb3307ceb58dec25
raks-hana <161952108+raks-hana@users.noreply.github.com>
2025-02-03 11:30:03 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+package Techmm;
+
+public class Calculator extends Application{
+    @Override
+    void read(){
+        System.out.println("Hello");
+    }
+    @Override
+    void calculate(){
+        System.out.println("Add");
+    }
+}
