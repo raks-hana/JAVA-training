1b99b04b4f98c6ed7d21b9e1cee350a1e5cf8893
raks-hana <161952108+raks-hana@users.noreply.github.com>
2025-02-08 22:26:40 +0530

    Create Division.java

---
+++ Week-2/Division.java
@@ -0,0 +1,24 @@
+package Techmm;
+
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter dividend: ");
+        int dividend = sc.nextInt();
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        int quotient = 0;
+        int remainder = dividend;
+
+        while (remainder >= divisor) {
+            remainder -= divisor;
+            quotient++;
+        }
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
