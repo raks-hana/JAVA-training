ebc67cc34a94e6937eff1c33e67ab91e36089d7d
raks-hana <161952108+raks-hana@users.noreply.github.com>
2025-02-10 08:20:32 +0530

    Create Utility-2.java

---
+++ Week-2/Utility/Utility-2.java
@@ -0,0 +1,8 @@
+import utility.NumberUtils;
+
+public class UtilityDemo {
+    public static void main(String[] args) {
+        int number = 12345;
+        System.out.println("Length of " + number + " is: " + NumberUtils.findLength(number));
+    }
+}
