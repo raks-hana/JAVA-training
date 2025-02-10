418d048ca32f019fc946f5b44f818c0a331f9e3f
raks-hana <161952108+raks-hana@users.noreply.github.com>
2025-02-10 14:20:56 +0530

    Create Ex-1-java-IO.java

---
+++ 10-2-2025/Ex-1-java-IO.java
@@ -0,0 +1,16 @@
+package Techmm;
+
+public class ExcersiseOneIO {
+    public static void main(String[] args) {
+        System.out.print("Hi! ");
+        System.out.print("Welcome ");
+
+        System.out.println("Player!");
+        System.out.println("Begin Adventure");
+        System.out.println("Enter Dungeon");
+
+        System.err.print("Oops! You have died, ");
+        System.err.print("Try again, Player...");
+    }
+}
+
