5e0a39c6e951503f401df6879b2d9095d57585c6
raks-hana <161952108+raks-hana@users.noreply.github.com>
2025-02-15 15:52:55 +0530

    Create Task10.js

---
+++ 14-2-2025/Task10.js
@@ -0,0 +1,12 @@
+var mark = 52
+if(mark==100){
+    console.log("Phenomenal!")
+}else if(mark>=85){
+    console.log("Excellent!")
+}else if(mark>=71 && mark<=85){
+    console.log("Good work!")
+}else if(mark>=51 && mark<=70){
+    console.log("Practice well!")
+}else{
+    console.log("Hard work is needed")
+}
