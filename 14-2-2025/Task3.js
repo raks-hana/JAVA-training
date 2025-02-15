26236cb8468b4f5baccf35fa6d7701a9e5a74c0b
raks-hana <161952108+raks-hana@users.noreply.github.com>
2025-02-15 14:43:26 +0530

    Create Task3.js

---
+++ 14-2-2025/Task3.js
@@ -0,0 +1,11 @@
+const a = 7
+for(let i=2;i<a;i++){
+    if(a%i==0){
+        console.log("The number is not prime");
+        break
+    }
+    else{
+        console.log("The number is prime");
+        break
+    }
+}
